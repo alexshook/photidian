63cbe3fa000dfdb305b97855ea54b34520adfce2
alexshook <alexandra.shook@gmail.com>
2014-05-11 21:38:04 -0400

    devise sanitizer sign up bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:account_update).push(:username, :first_name, :last_name, :avatar)
+    devise_parameter_sanitizer.for(:sign_up).push(:username, :first_name, :last_name, :avatar)
   end
 
 end
