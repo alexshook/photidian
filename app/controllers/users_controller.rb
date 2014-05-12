1226670ea9ff6c95dc503fa337bf6a90364bb356
alexshook <alexandra.shook@gmail.com>
2014-05-11 21:11:54 -0400

    s3 erroring out on missing bucket option

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
 class UsersController < ApplicationController
 
   def index
+    @users = User.all
+  end
+
+  def show
+    @user = current_user
   end
 
 private
   def user_params
-    params.require(:user).permit(:username, :first_name, :last_name)
+    params.require(:user).permit(:username, :first_name, :last_name, :avatar)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Photodian::Application.routes.draw do
   root to: 'pages#index'
   devise_for :users
 
+  resources :users, only: [:index, :show]
   resources :photos
 end
