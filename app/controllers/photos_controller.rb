8c1e6e154dba67edd486a103ed377f6bdeccfb16
alexshook <alexandra.shook@gmail.com>
2014-05-13 14:29:25 -0400

    add s3 methods for file upload

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,38 @@
 class PhotosController < ApplicationController
 
+  def index
+    render json: {
+      policy: s3_upload_policy_document,
+      signature: s3_upload_signature,
+      key: "uploads/#{SecureRandom.uuid}/#{params[:doc][:title]}",
+      success_action_redirect: '/'
+    }
+  end
+
+  def s3_upload_policy_document
+    Base64.encode64(
+    {
+      expiration: 30.minutes.from_now.utc.strftime('%Y-%m-%dT%H:%M:%S.000Z'),
+      conditions: [
+        { bucket: ENV['PHOTIDIAN_BUCKET_NAME'] },
+        { acl: 'public-read' },
+        ["starts-with", "$key", "uploads/"],
+        { success_action_status: '201' }
+        ]
+      }.to_json
+    ).gsub(/\n|\r/, '')
+  end
+
+  def s3_upload_signature
+    Base64.encode64(
+      OpenSSL::HMAC.digest(
+        OpenSSL::Digest::Digest.new('sha1'),
+        ENV['AWS_SECRET_KEY_ID'],
+        s3_upload_policy_document
+      )
+    ).gsub(/\n/, '')
+  end
+
   def save_photo
     # from the AWS S3 documentation
       # access environment variables for AWS S3
@@ -12,7 +45,6 @@ class PhotosController < ApplicationController
       data = params[:file]
       # do i need to set the file name? file_name? file?
       # S3Object.store(data, open(data), 'photidian')
-      obj = bucket.objects['key'].write(data)
 
     respond_to do |format|
       format.json { render json: return_data.to_json }
