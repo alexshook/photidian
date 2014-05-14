ed829173454ac1efb68de44cd96fd2210812d531
alexshook <alexandra.shook@gmail.com>
2014-05-14 18:57:37 -0400

    switching class method from photo to user

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -7,7 +7,7 @@ class PhotosController < ApplicationController
   def save_photo
     # from the AWS S3 documentation
       # access environment variables for AWS S3
-      s3 = Photo.aws_request
+      s3 = User.aws_request
       # grab the bucket for photidian app
       bucket = s3.buckets[ENV['PHOTIDIAN_BUCKET_NAME']]
       # update the bucket ACL (access control)
@@ -15,7 +15,7 @@ class PhotosController < ApplicationController
       # get data from the js request
       data = params[:file]
       # name the file with username and current date and time
-      file_name = "#{current_user.username}/" + Time.now.to_s.gsub(" ", "_") + ".jpg"
+      file_name = "#{current_user.username}/" + Date.today.to_s.gsub(" ", "_") + ".jpg"
       # create the object for s3
       bucket.objects.create(file_name, data)
       # check response from s3
--- app/models/user.rb
+++ app/models/user.rb
@@ -23,9 +23,14 @@ class User < ActiveRecord::Base
     {:bucket => ENV['PHOTIDIAN_BUCKET_NAME'], :access_key_id => ENV['AWS_ACCESS_KEY_ID'], :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']}
   end
 
+  # configure AWS environment variables for S3
+  def self.aws_request
+    return AWS::S3.new(:access_key_id => ENV['AWS_ACCESS_KEY_ID'], :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'])
+  end
+
   # get objects from s3, read objects so they can display as images
   def self.get_photos(user)
-    s3 = Photo.aws_request
+    s3 = User.aws_request
     bucket = s3.buckets[ENV['PHOTIDIAN_BUCKET_NAME']]
 
     data_array = []
@@ -35,6 +40,9 @@ class User < ActiveRecord::Base
     return data_array
   end
 
+  def order_photos
+
+  end
 end
 
 # need to handle getting photos a different way-- don't use current user because it can only get the current user's files
