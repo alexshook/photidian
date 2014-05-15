b9e01d8627c559b3f754737a50fdfad4a4e574dd
alexshook <alexandra.shook@gmail.com>
2014-05-15 14:41:20 -0400

    jquery and css styles

--- app/models/follows.rb
+++
@@ -1,8 +0,0 @@
-class Follow < ActiveRecord::Base
-  # associations
-  belongs_to :follower, class_name: "User"
-  belongs_to :following, class_name: "User"
-
-  validates :follower_id, :following_id, presence: :true
-
-end
--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,6 @@
 class Relationship < ActiveRecord::Base
   belongs_to :follower, class_name: "User"
   belongs_to :following, class_name: "User"
+
+  validates :follower_id, :following_id, presence: :true
 end
