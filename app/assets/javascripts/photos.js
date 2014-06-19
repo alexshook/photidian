f23b3486ac309e6e9b78dedfef5944c87e6c7413
alexshook <alexandra.shook@gmail.com>
2014-06-19 15:48:24 -0400

    changing photo slideshow js to show first photo

--- app/assets/javascripts/photos.js
+++ app/assets/javascripts/photos.js
@@ -5,7 +5,7 @@ $().ready(function() {
     $("#user-camera-photos").removeClass('whiteout');
     $("#user-camera-photos > img").removeClass('whiteout');
     $(".photo > img").removeClass('photo');
-    // $("#user-camera-photos > div:first").fadeIn(1000);
+    $("#user-camera-photos > div:first").fadeIn(1000);
     setInterval(function() {
       $('#user-camera-photos > div:first')
         .fadeOut(1000)
