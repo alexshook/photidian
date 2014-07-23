0d5419d6370cfd2f1a6ac1175c8c5f15f43b9dac
alexshook <alexandra.shook@gmail.com>
2014-07-23 15:20:22 -0400

    removing unneeded search functions because the search form is always displayed, beginning support for modals

--- app/assets/javascripts/views/navView.js
+++ app/assets/javascripts/views/navView.js
@@ -2,17 +2,18 @@ var NavView = Backbone.View.extend ({
 
   initialize: function() {
     console.log('initialize NavView');
+    this.modal = {};
+    this.overlay = $('#overlay');
+    this.signInModal = $('#sign-in-modal');
+    this.signUpModal = $('#sign-up-modal');
+    this.content = $('#content');
+    this.close = $('#close');
   },
 
   events: {
-    'click a#search-users': 'displaySearchForm',
-    'click a#about': 'displayAbout'
-  },
-
-  displaySearchForm: function(e) {
-    e.preventDefault();
-    console.log('hey im displaySearchForm');
-    $('#search-form-wrapper').toggleClass('hide');
+    'click a#about': 'displayAbout',
+    'click a#sign-up-link': 'displaySignUpModal',
+    'click a#sign-in-link': 'displaySignInModal'
   },
 
   displayAbout: function(e) {
@@ -22,6 +23,18 @@ var NavView = Backbone.View.extend ({
     $('#index').empty();
     $('#index').append('<div id="about-template-wrapper"></div>');
     $('#about-template-wrapper').html(aboutTemplate);
+  },
+
+  displaySignUpModal: function(e, settings) {
+    e.preventDefault();
+    console.log('sign up modal!');
+    this.signUpModal.removeClass('hide');
+    this.overlay.removeClass('hide');
+  },
+
+  displaySignInModal: function(e) {
+    e.preventDefault();
+    console.log('sign in modal!');
   }
 
 });
